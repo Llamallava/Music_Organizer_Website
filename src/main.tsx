2676c1c62e819b56dca9991ecedfaf1fa581cfda
Matthew Eberhart <mattheweberhart75@gmail.com>
2026-06-11 11:18:38 -0700

    Routing issue

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Music_Organizer_Website">
       <App />
     </BrowserRouter>
   </StrictMode>,
